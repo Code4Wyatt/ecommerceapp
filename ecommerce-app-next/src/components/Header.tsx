5b4bbe8be23656e85a5650829da1180a1ed42f3f
Paul <pajmurray@outlook.com>
2023-03-31 23:23:31 +0100

    add subheader

--- ecommerce-app-next/src/components/Header.tsx
+++ ecommerce-app-next/src/components/Header.tsx
@@ -30,6 +30,22 @@ function Header() {
                     </div>
                 </div>
             </div>
+
+            <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-small text-white">
+                <p className="link flex items-center">
+                    <Bars3Icon className="h-6 mr-1" />
+                    All
+                </p>
+                <p className="link">Prime Video</p>
+                <p className="link">Amazon Business</p>
+                <p className="link">Todays's Deals</p>
+                <p className="link hidden lg:inline-flex">Electronics</p>
+                <p className="link hidden lg:inline-flex">Food & Grocery</p>
+                <p className="link hidden lg:inline-flex">Prime</p>
+                <p className="link hidden lg:inline-flex">Buy Again</p>
+                <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+                <p className="link hidden lg:inline-flex">Health & Personal Care</p>
+            </div>
         </header>
     )
 }
